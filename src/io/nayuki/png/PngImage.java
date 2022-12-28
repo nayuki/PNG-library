0006e90b1716049b1d3378f3a563bc36035d549f
Project Nayuki <me@nayuki.io>
2022-12-28 04:15:43 +0000

    Implemented PngImage.read(), which is a higher level than RawPng.read().

--- src/io/nayuki/png/PngImage.java
+++ src/io/nayuki/png/PngImage.java
@@ -8,10 +8,13 @@
 
 package io.nayuki.png;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +26,18 @@ import io.nayuki.png.chunk.Ihdr;
 
 public final class PngImage {
 	
+	public static PngImage read(File inFile) throws IOException {
+		try (var in = new BufferedInputStream(new FileInputStream(inFile))) {
+			return read(in);
+		}
+	}
+	
+	
+	public static PngImage read(InputStream in) throws IOException {
+		return new PngImage(RawPng.read(in));
+	}
+	
+	
 	public Optional<Ihdr> ihdr = Optional.empty();
 	
 	public List<Chunk> beforeIdats = new ArrayList<>();
@@ -30,6 +45,38 @@ public final class PngImage {
 	public List<Idat> idats = new ArrayList<>();
 	
 	public List<Chunk> afterIdats = new ArrayList<>();
+	
+	
+	public PngImage() {}
+	
+	
+	private PngImage(List<Chunk> chunks) {
+		boolean hasIend = false;
+		for (Chunk chunk : chunks) {
+			if (hasIend)
+				throw new IllegalArgumentException();
+			else if (chunk instanceof Iend)
+				hasIend = true;
+			else if (ihdr.isEmpty()) {
+				if (chunk instanceof Ihdr chk)
+					ihdr = Optional.of(chk);
+				else
+					throw new IllegalArgumentException();
+			} else if (chunk instanceof Ihdr)
+				throw new IllegalArgumentException();
+			else if (chunk instanceof Idat chk) {
+				if (afterIdats.isEmpty())
+					idats.add(chk);
+				else
+					throw new IllegalArgumentException();
+			} else if (idats.isEmpty())
+				beforeIdats.add(chunk);
+			else
+				afterIdats.add(chunk);
+		}
+		if (ihdr.isEmpty() || idats.isEmpty() || !hasIend)
+			throw new IllegalArgumentException();
+	}
 	
 	
 	public void write(File outFile) throws IOException {
