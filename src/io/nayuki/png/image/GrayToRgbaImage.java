dc05b9ec8cb16269fc3694406c53a51bf34ec42b
Project Nayuki <me@nayuki.io>
2023-02-16 05:49:35 +0000

    Added gray-to-RGBA image adapter class.

---
+++ src/io/nayuki/png/image/GrayToRgbaImage.java
@@ -0,0 +1,48 @@
+/* 
+ * PNG library (Java)
+ * 
+ * Copyright (c) Project Nayuki
+ * MIT License. See readme file.
+ * https://www.nayuki.io/page/png-library
+ */
+
+package io.nayuki.png.image;
+
+import java.util.Objects;
+
+
+/**
+ * Adapts a grayscale-alpha image to a red-green-blue-alpha image, read-only.
+ */
+public final class GrayToRgbaImage implements RgbaImage {
+	
+	private final GrayImage image;
+	
+	
+	public GrayToRgbaImage(GrayImage img) {
+		image = Objects.requireNonNull(img);
+	}
+	
+	
+	@Override public int getWidth() {
+		return image.getWidth();
+	}
+	
+	
+	@Override public int getHeight() {
+		return image.getHeight();
+	}
+	
+	
+	@Override public int[] getBitDepths() {
+		int[] temp = image.getBitDepths();
+		return new int[]{temp[0], temp[0], temp[0], temp[1]};
+	}
+	
+	
+	@Override public long getPixel(int x, int y) {
+		int temp = image.getPixel(x, y);
+		return ((temp >>> 16) * 0x0001000100010000L) | (temp & 0xFFFF);
+	}
+	
+}
