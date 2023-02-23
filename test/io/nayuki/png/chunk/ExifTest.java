2c622960eb30cefbd2db804526b33f6374285644
Project Nayuki <me@nayuki.io>
2023-02-23 02:31:28 +0000

    Added test suites for chunk types: dSIG, eXIf.

---
+++ test/io/nayuki/png/chunk/ExifTest.java
@@ -0,0 +1,28 @@
+/* 
+ * PNG library (Java)
+ * 
+ * Copyright (c) Project Nayuki
+ * MIT License. See readme file.
+ * https://www.nayuki.io/page/png-library
+ */
+
+package io.nayuki.png.chunk;
+
+import org.junit.Test;
+import io.nayuki.png.TestUtil;
+
+
+public final class ExifTest {
+	
+	@Test public void testGetData() {
+		// All examples are random data and not real Exif profiles
+		TestUtil.assertDataEquals("19C4EAEC2AA8CC7BB5B17DFDCDF665EECE5B", new Exif(TestUtil.hexToBytes("19C4EAEC2AA8CC7BB5B17DFDCDF665EECE5B")));
+	}
+	
+	
+	@Test public void testWriteChunk() {
+		// All examples are random data and not real Exif profiles
+		TestUtil.assertChunkBytesEqual("00000009 65584966 C2ABF509891AACCB00 5C420B5B", new Exif(TestUtil.hexToBytes("C2ABF509891AACCB00")));
+	}
+	
+}
