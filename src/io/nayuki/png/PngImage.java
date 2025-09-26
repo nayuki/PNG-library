afb756b2b4c47407ccf5285e00440a986f0f21ac
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2025-09-26 20:22:06 +0200

    Remove unused import

--- src/io/nayuki/png/PngImage.java
+++ src/io/nayuki/png/PngImage.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
